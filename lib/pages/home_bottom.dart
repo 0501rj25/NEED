70d658b0720341939a4128ef3e63867f1f0df3db
Ramesh jha <rjprusys@gmail.com>
2022-03-11 10:38:55 +0530

    bottombar and home page complete

--- lib/pages/home_bottom.dart
+++ lib/pages/home_bottom.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class MyHomeBottom extends StatefulWidget {
   const MyHomeBottom({Key? key}) : super(key: key);
@@ -8,8 +9,10 @@ class MyHomeBottom extends StatefulWidget {
 }
 
 class _MyHomeBottomState extends State<MyHomeBottom> {
+  DateTime currentdate = DateTime.now();
   @override
   Widget build(BuildContext context) {
+    String _formattedate = DateFormat.yMMMd().format(currentdate);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
@@ -24,16 +27,52 @@ class _MyHomeBottomState extends State<MyHomeBottom> {
                     padding: const EdgeInsets.only(top: 50.0, left: 30.0),
                     height: 100,
                     width: MediaQuery.of(context).size.width,
-                    //color: Colors.amber,
-                    child: const Text(
-                      "Today",
-                      style: TextStyle(
+                    child: Text(
+                      'Today\n$_formattedate. ',
+                      style: const TextStyle(
                           color: Colors.white,
                           fontWeight: FontWeight.bold,
                           fontSize: 20.0,
                           fontStyle: FontStyle.normal),
                     ),
                   ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.vertical,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.only(left: 30),
+                            height: 200,
+                            width: MediaQuery.of(context).size.width,
+                            child: RichText(
+                              text: TextSpan(
+                                style: Theme.of(context).textTheme.bodyLarge,
+                                children: const [
+                                  //TextSpan(text: 'Created with '),
+                                  WidgetSpan(
+                                    child: Padding(
+                                      padding:
+                                          EdgeInsets.symmetric(horizontal: 2.0),
+                                      child: Icon(Icons.accessibility),
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: '  Challenges',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20.0),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
                 ],
               ),
             ],

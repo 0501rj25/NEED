b5180fade8abe929a01c7d8cdda77b68269c890a
Ramesh jha <rjprusys@gmail.com>
2022-03-19 11:56:45 +0530

    DAY-8 COMMIT

--- lib/pages/home_bottom.dart
+++ lib/pages/home_bottom.dart
@@ -45,13 +45,12 @@ class _MyHomeBottomState extends State<MyHomeBottom> {
                         children: [
                           Container(
                             padding: const EdgeInsets.only(left: 30),
-                            height: 200,
+                            height: 30,
                             width: MediaQuery.of(context).size.width,
                             child: RichText(
                               text: TextSpan(
                                 style: Theme.of(context).textTheme.bodyLarge,
                                 children: const [
-                                  //TextSpan(text: 'Created with '),
                                   WidgetSpan(
                                     child: Padding(
                                       padding:
@@ -60,7 +59,7 @@ class _MyHomeBottomState extends State<MyHomeBottom> {
                                     ),
                                   ),
                                   TextSpan(
-                                    text: '  Challenges',
+                                    text: 'Challenges',
                                     style: TextStyle(
                                         fontWeight: FontWeight.bold,
                                         fontSize: 20.0),
@@ -68,6 +67,84 @@ class _MyHomeBottomState extends State<MyHomeBottom> {
                                 ],
                               ),
                             ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              height: 170,
+                              width: 340,
+                              decoration: BoxDecoration(
+                                  image: const DecorationImage(
+                                    image:
+                                        AssetImage("assets/700_challenge.png"),
+                                    fit: BoxFit.cover,
+                                  ),
+                                  borderRadius: BorderRadius.circular(10)),
+                              padding: const EdgeInsets.only(top: 135),
+                              child: RichText(
+                                text: const TextSpan(
+                                  children: [
+                                    WidgetSpan(
+                                      child: Padding(
+                                        padding: EdgeInsets.symmetric(
+                                            horizontal: 2.0),
+                                        child: Icon(Icons.accessibility),
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text: 'Step up your fitness game',
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 15.0),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(top: 4.0, left: 5.0),
+                            child: SizedBox(
+                              height: 20,
+                              width: 340,
+                              child: Text(
+                                "Hey Ramesh,heres your progress for the day",
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              SizedBox(
+                                width: 260,
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      height: 150,
+                                      width: 70,
+                                      decoration: const BoxDecoration(
+                                          color: Colors.amber,
+                                          shape: BoxShape.circle),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(
+                                width: 100,
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      height: 150,
+                                      width: 70,
+                                      decoration: const BoxDecoration(
+                                          color: Colors.amber,
+                                          shape: BoxShape.circle),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
